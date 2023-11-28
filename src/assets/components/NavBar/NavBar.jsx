a57b7b73e8b65712416c18b6a67fb6b39ac258bb
MarceloFVillanueva89 <marcelofvillanueva89@gmail.com>
2023-11-28 00:22:56 -0300

    Modificaciones varioas

--- src/assets/components/NavBar/NavBar.jsx
+++ src/assets/components/NavBar/NavBar.jsx
@@ -4,27 +4,47 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { getProducts } from "../../../asyncMock";
 import imagen from '../../../assets/react.svg'
+import { useState, useEffect } from "react";
 
 import './NavBar.css'
 
-
 function NavBar() {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(()=> {
+    getProducts()
+    .then((res) => {
+      setProducts(res)
+    })
+    .catch((err) =>{
+      console.log(err)
+    })
+  }, [])
+
   return (
     <>
       <Navbar expand="lg"  bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home"><img src={imagen} / ></Navbar.Brand>
+          <Navbar.Brand href="/"><img src={imagen} / ></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
               <NavDropdown title="Categorias" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Hombres</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Mujeres</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Niños</NavDropdown.Item>
+                {
+                  products.length > 0  &&
+                  products.map((product)=>{
+                    let hrefs = "#action/3."+product.id
+                    return (
+                      <NavDropdown.Item key={product.id} href={hrefs}>{product.category}</NavDropdown.Item>
+                    )
+                  })
+                }
               </NavDropdown>
-              <Nav.Link href="#link">Nosotros</Nav.Link>
+              <Nav.Link href="/nosotros">Nosotros</Nav.Link>
             </Nav>
             <CartWidget/>
           </Navbar.Collapse>
